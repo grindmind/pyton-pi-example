f36d918236eda4493321b833e357e39534e7c808
mrrobot <bobbyhockey93@gmail.com>
2019-08-02 13:24:52 -0400

    new branch

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 
 #!/usr/bin/python3
 
-def main(msg);
+def main(msg):
+    // No longer need comments here
     // Print a message
     print(msg)
+
+def seconday():
+    print("New function")
 
 
 main("Hi Friend")
